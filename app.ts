878322f441c1e865b646a3adcb9038ca2b1653b3
jdflemin <jdf@mchsi.com>
2017-05-23 11:19:50 -0700

    update

--- app.ts
+++ app.ts
@@ -11,11 +11,23 @@ require('dotenv').config({silent: true});
 import courses from './api/course';
 import lessons from './api/lesson';
 
+//const CONNECTION = 'mongodb://angel:password@ds159050.mlab.com:59050/codercampsdb--angel';
+
 mongoose.connect(process.env.CONNECTION_STRING)
   .then(() => console.log('connection established'))
   .catch((err) => console.log(err));
 
+import routes from './routes/index';
+import users from './routes/users';
+
 let app = express();
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
